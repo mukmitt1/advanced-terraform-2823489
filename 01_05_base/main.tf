f8e8d068a9131568e703a857ec38a2e3862e190c
mukmitt1 <76959601+mukmitt1@users.noreply.github.com>
2022-12-25 14:38:48 -0500

    fix

--- 01_05_base/main.tf
+++ 01_05_base/main.tf
@@ -119,7 +119,7 @@ resource "aws_eip" "one" {
   vpc                       = true
   network_interface         = aws_network_interface.web-server-nic.id
   associate_with_private_ip = "172.16.1.50"
-  depends_on                = [aws_internet_gateway.gw]
+  depends_on                = [aws_internet_gateway.gateway1]
 }
 
 
