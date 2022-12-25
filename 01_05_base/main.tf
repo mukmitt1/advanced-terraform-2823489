6b9c27ac3ff876437e939fc2267f50378a0258e1
mukmitt1 <76959601+mukmitt1@users.noreply.github.com>
2022-12-25 14:35:59 -0500

    fix

--- 01_05_base/main.tf
+++ 01_05_base/main.tf
@@ -112,7 +112,7 @@ resource "aws_security_group" "sg-nodejs-instance" {
 resource "aws_network_interface" "web-server-nic" {
   subnet_id       = aws_subnet.subnet-1.id
   private_ips     = ["172.16.1.50"]
-  security_groups = [aws_security_group.allow_web.id]
+  security_groups = [aws_security_group.sg-nodejs-instance.id]
 }
 
 resource "aws_eip" "one" {
