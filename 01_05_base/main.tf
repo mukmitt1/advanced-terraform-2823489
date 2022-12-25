c8e91d000f0796edfe166b77623e0fab897cb1ce
mukmitt1 <76959601+mukmitt1@users.noreply.github.com>
2022-12-25 14:02:36 -0500

    instance update

--- 01_05_base/main.tf
+++ 01_05_base/main.tf
@@ -115,13 +115,6 @@ resource "aws_instance" "nodejs1" {
   subnet_id = aws_subnet.subnet1.id
   vpc_security_group_ids = [aws_security_group.sg-nodejs-instance.id]
   key_name               = var.ssh_key_name
-
-  connection {
-    type        = "ssh"
-    host        = self.public_ip
-    user        = "ec2-user"
-    #private_key = file(var.private_key_path)
-  }
 }
 
 
