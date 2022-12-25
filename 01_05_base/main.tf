537a73314c288d79360312026d1d4c23c8c95656
mukmitt1 <76959601+mukmitt1@users.noreply.github.com>
2022-12-25 12:55:05 -0500

    Update main.tf

--- 01_05_base/main.tf
+++ 01_05_base/main.tf
@@ -110,7 +110,8 @@ resource "aws_security_group" "sg-nodejs-instance" {
 
 # INSTANCE
 resource "aws_instance" "nodejs1" {
-  ami = data.aws_ami.aws-linux.id
+  #ami = data.aws_ami.aws-linux.id
+  ami = "ami-0b5eea76982371e91"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.subnet1.id
   vpc_security_group_ids = [aws_security_group.sg-nodejs-instance.id]
